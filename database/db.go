1764e6407dc751bedb9d3d55827fe8f6344c70bc
John Latties <johnlatties@gmail.com>
2024-06-22 17:43:50 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -1,12 +1,13 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"fmt"
 )
 
 var (
@@ -24,7 +25,7 @@ func ConectaComBancoDeDados() {
 	log.Printf("Conectando ao banco de dados com as seguintes configurações:\nHost: %s\nUser: %s\nDBName: %s\nPort: %s\n", host, user, dbname, port)
 
 	stringDeConexao := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, dbname, port)
-	DB, err := gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Erro ao conectar com banco de dados: %v", err)
 	}
