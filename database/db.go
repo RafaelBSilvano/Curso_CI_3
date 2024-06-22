b5de575ce4d334592d627e30791b477ecc883e34
RafaelBSilvano <rafael.braga.silvano@gmail.com>
2024-06-22 17:16:52 -0300

    Atualiza db.go

--- database/db.go
+++ database/db.go
@@ -14,12 +14,18 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" "
-	DB, err = gorm.Open(postgres.Open(stringDeConexao)) 
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	host := os.Getenv("HOST")
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	dbname := os.Getenv("DBNAME")
+	port := os.Getenv("DBPORT")
+
+	log.Printf("Conectando ao banco de dados com as seguintes configurações:\nHost: %s\nUser: %s\nDBName: %s\nPort: %s\n", host, user, dbname, port)
+
+	stringDeConexao := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", host, user, password, dbname, port)
+	DB, err := gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Fatalf("Erro ao conectar com banco de dados: %v", err)
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
